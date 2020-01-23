2457699e9c69ae93170e9ec8c911d535425cf18b
Vanja Radovanović <elvanja@gmail.com>
2020-01-23 17:48:30 +0100

    Move cluster membership to dedicated module

---
+++ lib/abbr/cluster.ex
@@ -0,0 +1,35 @@
+defmodule Abbr.Cluster do
+  @moduledoc """
+  Makes a node leave or join the cluster.
+  This is achieved by (ab)using libcluster and it's supervisor a bit.
+  That's because we use gossip protocol which would normally just reconnect on next tick.
+  """
+
+  alias Abbr.ClusterSupervisor
+  alias Cluster.Strategy
+
+  @spec leave :: :ok | {:error, String.t()}
+  def leave do
+    with :ok <- Supervisor.terminate_child(ClusterSupervisor, :local),
+         :ok <-
+           Strategy.disconnect_nodes(
+             :local,
+             {:erlang, :disconnect_node, []},
+             {:erlang, :nodes, [:connected]},
+             Node.list()
+           ) do
+      :ok
+    else
+      _ -> {:error, "could not leave cluster"}
+    end
+  end
+
+  @spec join :: :ok | {:error, String.t()}
+  def join do
+    case Supervisor.restart_child(ClusterSupervisor, :local) do
+      {:ok, _} -> :ok
+      {:error, :running} -> :ok
+      _ -> {:error, "could not join cluster"}
+    end
+  end
+end
--- lib/abbr_web/controllers/cluster_controller.ex
+++ lib/abbr_web/controllers/cluster_controller.ex
@@ -1,30 +1,20 @@
 defmodule AbbrWeb.ClusterController do
   use AbbrWeb, :controller
 
-  alias Abbr.ClusterSupervisor
-  alias Cluster.Strategy
+  alias Abbr.Cluster
 
   action_fallback AbbrWeb.FallbackController
 
   def leave(conn, _) do
-    with :ok <- Supervisor.terminate_child(ClusterSupervisor, :local),
-         :ok <-
-           Strategy.disconnect_nodes(
-             :local,
-             {:erlang, :disconnect_node, []},
-             {:erlang, :nodes, [:connected]},
-             Node.list()
-           ) do
-      send_resp(conn, 200, "")
-    else
+    case Cluster.leave() do
+      :ok -> send_resp(conn, 200, "")
       _ -> send_resp(conn, 500, "")
     end
   end
 
   def join(conn, _) do
-    case Supervisor.restart_child(ClusterSupervisor, :local) do
-      {:ok, _} -> send_resp(conn, 200, "")
-      {:error, :running} -> send_resp(conn, 200, "")
+    case Cluster.join() do
+      :ok -> send_resp(conn, 200, "")
       _ -> send_resp(conn, 500, "")
     end
   end
