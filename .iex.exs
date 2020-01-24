f5fef317363581f5dd04e4bd5b99ec6da8453706
Vanja Radovanović <elvanja@gmail.com>
2020-01-24 13:05:06 +0100

    Alias stuff automatically in REPL

---
+++ .iex.exs
@@ -0,0 +1,9 @@
+alias Abbr.Cache
+alias Abbr.Cluster
+alias Abbr.ClusterCache.Monitor, as: ClusterCacheMonitor
+alias Abbr.ClusterCache.SyncOnStartup, as: ClusterCacheSyncOnStartup
+alias Abbr.Expand
+alias Abbr.Health
+alias Abbr.LocalCache
+alias Abbr.Shorten
+alias Abbr.Url
